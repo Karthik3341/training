a6a7eff5fa35c9c68c19c9c11282c16f81e0d408
kart3341 <kart3341@CSLH3142.global.chainsys.com>
2022-11-17 16:50:51 +0530

    commit

---
+++ src/com/chainsys/day5/BasicArray22.java
@@ -0,0 +1,16 @@
+package com.chainsys.day5;
+
+public class BasicArray22 {
+
+	public static void main(String[] args) {
+String[] name=new String[7];
+name[0]="ABC";
+name[1]="Koki";
+name[2]="Saran";
+for(int i=0;i<name.length;i++)
+{
+	System.out.println(name[i]);
+}
+	}
+
+}
