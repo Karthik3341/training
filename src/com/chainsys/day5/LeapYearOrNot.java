659743ae342b0140f2cf2ca03a7c4d17eb796ce9
kart3341 <kart3341@CSLH3142.global.chainsys.com>
2022-11-17 17:53:14 +0530

    commit

---
+++ src/com/chainsys/day5/LeapYearOrNot.java
@@ -0,0 +1,23 @@
+package com.chainsys.day5;
+
+public class LeapYearOrNot {
+
+	public static void main(String[] args) {
+int year=1956;
+for(int i=0;i<=0;i++)
+{
+	if(year%400==0)
+	{
+		System.out.println("The given year is leap");
+	}
+	else if(year%100 ==0) {
+		System.out.println("The given year is not leap");
+	}
+	else
+	{
+		System.out.println("The given year is not leap");
+	}
+}
+	}
+
+}
---
+++ src/com/chainsys/day5/OddorEvenLooping.java
@@ -0,0 +1,23 @@
+package com.chainsys.day5;
+
+public class OddorEvenLooping {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.println("The given number is odd:");
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 2 != 0) {
+				System.out.println(a[i]);
+			}
+		}
+		System.out.println("The given number is even:");
+		{
+			for (int i = 0; i < a.length; i++) {
+				if (a[i] % 2 == 0) {
+					System.out.println(a[i]);
+				}
+			}
+		}
+	}
+
+}
