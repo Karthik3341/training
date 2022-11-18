1bbbd52d7bfc929aae1bf61d7208fc2cd3c4c888
kart3341 <kart3341@CSLH3142.global.chainsys.com>
2022-11-18 09:31:20 +0530

    commit

---
+++ src/com/chainsys/day5/FibonacciLooping.java
@@ -0,0 +1,22 @@
+package com.chainsys.day5;
+
+import java.util.Scanner;
+
+public class FibonacciLooping {
+
+	public static void main(String[] args) {
+		int a = 0, b = 1, c;
+		Scanner ps = new Scanner(System.in);
+		System.out.println("Enter the count of Fibonacci Series:");
+		int count = ps.nextInt();
+
+		for (int i = 1; i <= count; i++) {
+			System.out.println(a);
+
+			c = b;
+			b = a;
+			a = b + c;
+		}
+
+	}
+}
